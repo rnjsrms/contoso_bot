c25d832f8c01620cde5e18485386cdd8fe9f487a
Daniel <rnjsrms@gmail.com>
2017-11-24 17:04:47 +1300

    Added more functionalities.l

--- Bot Application/Bot Application/Dialogs/LuisDialog.cs
+++ Bot Application/Bot Application/Dialogs/LuisDialog.cs
@@ -5,6 +5,10 @@ using Microsoft.Bot.Connector;
 using Microsoft.Bot.Builder.Luis;
 using Microsoft.Bot.Builder.Luis.Models;
 using System.Linq;
+using Bot_Application.Models;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Text;
 
 namespace Bot_Application.Dialogs
 {
@@ -15,7 +19,18 @@ namespace Bot_Application.Dialogs
         [LuisIntent("")]
         public async Task All(IDialogContext context, LuisResult result)
         {
+            var entities = result.Entities;
+            
             string message = $"Detected intent: " + string.Join(", ", result.Intents.Select(i => i.Intent));
+
+            if (entities.Count() > 0)
+            {
+                foreach (var ent in entities)
+                {
+                    message += "\n\n" + ent.Entity;
+                }
+            }
+            
             await context.PostAsync(message);
             context.Wait(MessageReceived);
         }
@@ -28,9 +43,69 @@ namespace Bot_Application.Dialogs
             context.Wait(MessageReceived);
         }
 
+        [LuisIntent("ExchangeRate")]
+        public async Task ExchangeRate(IDialogContext context, LuisResult result)
+        {
+            string currency = result.Entities.First().Entity.ToUpper();
+            string url = "https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=NZD&to_currency=" + currency + "&apikey=HZLXQ37YR6PGEMW7";
+
+            var client = new HttpClient();
+            HttpResponseMessage response;
+            response = await client.GetAsync(url);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var responseString = await response.Content.ReadAsStringAsync();
+
+                CurrencyExchangeModel responseModel = JsonConvert.DeserializeObject<CurrencyExchangeModel>(responseString);
+
+                double exchangerate = responseModel.RealtimeCurrencyExchangeRate.ExchangeRate;
+                string from_currency = responseModel.RealtimeCurrencyExchangeRate.From_CurrencyName;
+                string to_currency = responseModel.RealtimeCurrencyExchangeRate.To_CurrencyName;
+
+                string message = "1 " + from_currency + " equals " + exchangerate + " " + to_currency + "\n\n(data provided by www.alphavantage.co)";
+                await context.PostAsync(message);
+                context.Wait(MessageReceived);
+            }
+            else
+            {
+                var message = "Unable to fetch exchange rate data.\nPlease try again.";
+                await context.PostAsync(message);
+                context.Wait(MessageReceived);
+            }
+        }
+
+        [LuisIntent("Stock")]
+        public async Task Stock(IDialogContext context, LuisResult result)
+        {
+            string company = result.Entities.First().Entity.ToUpper();
+            string url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=" + company + "&apikey=HZLXQ37YR6PGEMW7";
+
+            var client = new HttpClient();
+            HttpResponseMessage response;
+            response = await client.GetAsync(url);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var responseString = await response.Content.ReadAsStringAsync();
+                var index1 = responseString.IndexOf("close", 286) + 4 + "close".Length;
+                var index2 = responseString.IndexOf('"', index1);
+                var length = index2 - index1;
+
+                var stock = responseString.Substring(index1, length);
+
+                string message = "Stock price for " + company + " is " + stock + " USD.\n\n(data provided by www.alphavantage.co)";
+                await context.PostAsync(message);
+                context.Wait(MessageReceived);
+            }
+            else
+            {
+                var message = "Unable to find stock data.\nPlease try again.";
+                await context.PostAsync(message);
+                context.Wait(MessageReceived);
+            }
 
 
-
-
+        }
     }
 }
---
+++ Bot Application/Bot Application/Models/CurrencyExchangeModel.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Bot_Application.Models
+{
+    [Serializable]
+    public class CurrencyExchangeModel
+    {
+        [JsonProperty(PropertyName = "Realtime Currency Exchange Rate")]
+        public RealtimeCurrencyExchangeRate RealtimeCurrencyExchangeRate { get; set; }
+    }
+
+    public class RealtimeCurrencyExchangeRate
+    {
+        [JsonProperty(PropertyName = "1. From_Currency Code")]
+        public string From_CurrencyCode { get; set; }
+
+        [JsonProperty(PropertyName = "2. From_Currency Name")]
+        public string From_CurrencyName { get; set; }
+
+        [JsonProperty(PropertyName = "3. To_Currency Code")]
+        public string To_CurrencyCode { get; set; }
+
+        [JsonProperty(PropertyName = "4. To_Currency Name")]
+        public string To_CurrencyName { get; set; }
+
+        [JsonProperty(PropertyName = "5. Exchange Rate")]
+        public double ExchangeRate { get; set; }
+
+        [JsonProperty(PropertyName = "6. Last Refreshed")]
+        public string LastRefreshed { get; set; }
+
+        [JsonProperty(PropertyName = "7. Time Zone")]
+        public string TimeZone { get; set; }
+    }
+}
